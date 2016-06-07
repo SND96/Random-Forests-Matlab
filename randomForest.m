ac5ed5c626a893bee6e02631d689be570a599739
Preetham Narayana Reddy <preethamnr1997@gmail.com>
2016-06-07 20:30:41 +0530

    Update randomForest.m
    Better modularised code

--- randomForest.m
+++ randomForest.m
@@ -1,4 +1,4 @@
-function [forests] = randomForest(examples, attributes)
+function [forests] = randomForest(examples, attributes, trees=10)
 
 numberAttr = length(attributes);
 numberEx = length(examples(:,1));
@@ -6,8 +6,7 @@ variables = ceil(sqrt(numberAttr));
 randEx = floor((632/1000)*numberEx);  % Number of random examples 
 activeEx = numberEx - randEx; % Examples left
 
-trees = 10; % Number of trees
-struct forests[10] = ('tree', 'oob');  % oob is Out of Bag error for each tree 
+struct forests[trees] = ('tree', 'oob');  % oob is Out of Bag error for each tree 
 
 subEx = zeros(randEx, variables+1); % Sub-matrix for examples
 testSet = zeros(activeEx, variables+1);
@@ -16,36 +15,11 @@ testSet = zeros(activeEx, variables+1);
 % Making the forest
 for i=1:trees
 
-    rAttr = sort(randperm(numberAttr, variables));
-    rEx = sort(randperm(numberEx, randEx);
-    
-    for y = 1:variables
-    subAttr(y) = attributes(rAttr(y));
-    end
-    
-    Subcount = 1;
-    testcount = 1;
-    for x=1:numberEx
-        if (any(find(x==rEx)) == 1)
-            for y=1:variables
-                subEx(Subcount ,y) = examples( x, rAttr(y));
-            end
-            subEx(Subcount ,variables+1) = examples( x , numberAttr+1);
-            Subcount = Subcount + 1;
-        
-        else
-            for y = 1:variables
-                testSet(testcount, y) = examples (x, rAttr(y));
-            end
-            testSet( testcount, y) = examples (x,rAttr(y));   
-            testcount = testcount +1;
-        end
-    end
-    
+    [subEx, testSet, subAttr] = randomSamping(examples, attributes ,randEx, activeEx);
     tree = ID3(subEx, subAttr, ones(variables));
     forests(i).tree = tree; % Should return a pointer to the tree
         
-    forests(i).oob = outOfBoxError( tree, testSet); % Calculate OOB for each tree
+    forests(i).oob = outOfBoxError(tree, testSet); % Calculate OOB for each tree
                
-end    
+    
 end
