458b873c0cbcc46c18727fb339e9d2ac8ace5842
Sahith Dambekodi <SND96@users.noreply.github.com>
2016-06-08 13:24:52 +0530

    Updated function call

--- randomForest.m
+++ randomForest.m
@@ -12,7 +12,7 @@ function [forests] = randomForest(examples, attributes, trees=10)
    % Making the forest
 for i=1:trees
     [subEx, testSet, subAttr] = randomSamping(examples, attributes ,randEx, activeEx);
-    tree = ID3(subEx, subAttr, ones(variables));
+    tree = MV_ID3(subEx, subAttr, ones(variables));
     forests(i).tree = tree; % Should return a pointer to the tree
     forests(i).oob = outOfBoxError(tree, testSet, subAttr); % Calculate OOB for each tree
 end  
