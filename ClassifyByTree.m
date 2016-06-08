105e69c7ab87106f2714a71fc8ef135e9a549514
Sahith Dambekodi <SND96@users.noreply.github.com>
2016-06-08 13:21:08 +0530

    Supports >=2 outcomes

--- ClassifyByTree.m
+++ ClassifyByTree.m
@@ -1,28 +1,16 @@
 function [classifications] = ClassifyByTree(tree, attributes, instance)
-
-    if( strcmp(tree.value, 'Iris-setosa') )
-        classifications = 'Iris-setosa';
-        return
-    end
-
-    if( strcmp(tree.value, 'Iris-versicolor') )
-        classifications = 'Iris-versicolor';
-        return
-    end
-
-    if( strcmp(tree.value, 'Iris-virginica') )
-        classifications = 'Iris-virginica';
-        return
-    end
-
-    index = find(ismember(attributes, tree.value) == 1)
-
-    % picking branch- need the ID3 coder to complete this file%
-    if (instance(1, index) <= tree.bound ); % attribute is true for this instance
-    % Recur down the right side
-    classifications = ClassifyByTree(tree.right, attributes, instance); 
+    attr = categorical(attributes);
+    actual = instance(1, length(instance));
+    if( sum(tree.value==attr) )
+        index = find(ismember(attributes, tree.value) == 1);
+        if (instance(1, index) <= tree.bound ); % attribute is true for this instance
+        % Recur down the right side
+        classifications = ClassifyByTree(tree.right, attributes, instance); 
+        else
+        % Recur down the left side
+        classifications = ClassifyByTree(tree.left, attributes, instance);
+        end
     else
-    % Recur down the left side
-    classifications = ClassifyByTree(tree.left, attributes, instance);
-    end
+        classifications = [tree.value, actual];  % Needed to check for accuracy
+    end    
 end
