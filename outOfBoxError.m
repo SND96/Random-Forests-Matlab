24fffb1f17da977f775bf61be25db56f143f5bf9
Preetham Narayana Reddy <preethamnr1997@gmail.com>
2016-06-08 11:47:52 +0530

    Create outOfBoxError.m

---
+++ outOfBoxError.m
@@ -0,0 +1,21 @@
+function [oob] = outOfBoxError(tree, testSet, subAttr)
+
+                    % 1 - Iris-setosa                 %
+                    % 2 - Iris-versicolor             %
+                    % 3 - Iris-virginica              %
+                    
+                    
+  actual = testSet(:, size(testSet,2) );
+  instance = testSet(:, 1:size(testSet,2)-1 );
+  correct = 0;
+  
+  
+  for i = 1:size(testSet,1)
+    prediction = ClassifyByTree(tree, subAttr, instance(i,:) );
+    if( strcmp( prediction, actual(i) ) == 0 )
+      correct = correct + 1;
+    end
+  end
+  
+  error = 1 - (correct / size(testSet,1));
+end
