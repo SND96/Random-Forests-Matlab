b4836bb9f9cea58a1cd7438530fc4a8fa01af78e
Sahith Dambekodi <SND96@users.noreply.github.com>
2016-06-08 13:00:52 +0530

    Fixed recurrence

--- ClassifyByTree.m
+++ ClassifyByTree.m
@@ -18,5 +18,11 @@ function [classifications] = ClassifyByTree(tree, attributes, instance)
     index = find(ismember(attributes, tree.value) == 1)
 
     % picking branch- need the ID3 coder to complete this file%
-
+    if (instance(1, index) <= tree.bound ); % attribute is true for this instance
+    % Recur down the right side
+    classifications = ClassifyByTree(tree.right, attributes, instance); 
+    else
+    % Recur down the left side
+    classifications = ClassifyByTree(tree.left, attributes, instance);
+    end
 end
