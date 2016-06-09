8fdeed6e49c7f67335740a84a2394bcba2b2a661
Preetham Narayana Reddy <preethamnr1997@gmail.com>
2016-06-09 12:26:05 +0530

    Update randomSampling.m

--- randomSampling.m
+++ randomSampling.m
@@ -1,26 +1,43 @@
-function [trainingSet, testSet, subAttr] = randomSampling(examples, attributes ,randEx, activeEx)
+function [trainingSet, testSet, subAttr] = randomSampling(examples, attributes ,randEx)
 
-  rAttr = sort(randperm(numberAttr, variables));
-  rEx = sort(randperm(numberEx, randEx);
-  numberAttr = length(attributes);
+%takes in examples (stacked as a row, last column being the classification)
+%         attributes- list of attributes -All attributes
+%         randEx - number of random examples to be selected from examples- Assumed the user gives a values < = numberEx
 
+%returns - training Set- row wise stacked randomly picked examples- last column is classification answer
+%        - testSet- row wise stacked examples that are ommited from the training Set
+%        - subAttr - column vector of attributes picked from the attributes vector.
+
+  numberAttr = length(attributes);  %number of attributes total
+  numberEx = size(examples,1);      %number of examples total
+  variables = ceil(sqrt(numberAttr)); % number of variables to be picked
+  rAttr = sort(randperm(numberAttr, variables)); % vector containing random numbers corresponding attributes to be picked
+  rEx = sort(randperm(numberEx, randEx);%  vector containing random numbers corresponding examples to be picked into test set
+  
+  %the following variables are to be returned
+  trainingSet = zeros(randEx,variables+1);
+  tesetSet = zeros(numberEx-randEx, variables+1);
+  subAttr = zeros(variables,1);
+  
+  
   for y = 1:variables
       subAttr(y) = attributes(rAttr(y));
   end
   
   Subcount = 1;
   testcount = 1;
-
-  for x=1:numberEx
-    if (any(find(x==rEx)) == 1)
+  
+  for x=1:numberEx  %go through all the examples
+    
+    if (any(find(x==rEx)) == 1)  %if index is in rEx
     
       for y=1:variables
-        subEx(Subcount ,y) = examples( x, rAttr(y));
+        subEx(Subcount ,y) = examples(x, rAttr(y));
       end
-      subEx(Subcount ,variables+1) = examples( x , numberAttr+1);
+      subEx(Subcount ,variables+1) = examples(x , numberAttr+1);
       Subcount = Subcount + 1;
   
-    else
+    else %else add to testSet
   
       for y = 1:variables
         testSet(testcount, y) = examples (x, rAttr(y));
