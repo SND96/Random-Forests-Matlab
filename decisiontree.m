558631ee291032c076d3572ba96b37d21287e67b
Preetham Narayana Reddy <preethamnr1997@gmail.com>
2016-06-09 17:45:53 +0530

    Update decisiontree.m

--- decisiontree.m
+++ decisiontree.m
@@ -1,29 +1,16 @@
-function [] = decisiontree(inputFileName)
+function [] = decisiontree()
 
-    formatSpec = '%f%f%f%f%C';    %specifies the input file format
-    ex = readtable(inputFileNamem 'Delimiter',',', 'Format',formatSpec); %opens the input file
+ex = load('irisdata.txt');
 
-    ex = table2cell(ex); 
-    for i=1:150   % Outcomes as numbers
-        if(strcmp('Iris-sentosa',ex{i,5}))
-            ex{i,5} = 1;
-        elseif(strcmp('Iris-virginica',ex(i,5)))
-            ex{i,5} = 2;
-        else
-            ex{i,5} = 3;
-        end        
-    end    
+attributes = ['sepal length', 'sepal width','petal length','petal width'];
+numAttributes = 4;
 
-    ex = cell2mat(ex);
-    attributes = ['sepal length', 'sepal width','petal length','petal width'];
-    numAttributes = 4;
+tree = ID3(ex, attributes, ones(numAttributes))
 
-    tree = MV_ID3(ex, attributes, ones(numAttributes))
+PrintTree(tree,null,null);  %wtf?
 
-    PrintTree(tree,*******);  %wtf?
-
-    for i = 1:150
-        outcome = ClassifyByTree(tree, attributes, ex(i,:));
-        fprintf("%f", outcome);
-    end
+for i = 1:150
+outcome = ClassifyByTree(tree, attributes, ex(i,:));
+fprintf(outcome);
+end
 end
