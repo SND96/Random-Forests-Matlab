0d5b9790b98552a1eee411abeba7aac78b3c69c1
Sahith Dambekodi <SND96@users.noreply.github.com>
2016-06-10 11:15:58 +0530

    Buggy

--- ClassifyByTree.m
+++ ClassifyByTree.m
@@ -1,15 +1,19 @@
 function [classifications] = ClassifyByTree(tree, attributes, instance)
     attr = categorical(attributes);
-    if( sum(tree.value==attr) )
+    if( sum(strcmp(tree.value,attr)) >0)
         index = find(ismember(attributes, tree.value) == 1);
         if (instance(1, index) <= tree.bound ); % attribute is true for this instance
         % Recur down the right side
+        fprintf('lef');
         classifications = ClassifyByTree(tree.right, attributes, instance); 
         else
         % Recur down the left side
+        
+        fprintf('rig');
         classifications = ClassifyByTree(tree.left, attributes, instance);
         end
     else
         classifications =tree.value;
+        return
     end    
 end
