b4cbf059ca726712d9eaf0679fffb8dcb07beda1
Sahith Dambekodi <SND96@users.noreply.github.com>
2016-06-10 23:05:18 +0530

    Update ClassifyByTree.m

--- ClassifyByTree.m
+++ ClassifyByTree.m
@@ -1,21 +1,16 @@
 function classifications = ClassifyByTree(tree, attributes, instance)
     
-    %fprintf('%d',strcmp(tree.value,attributes));
+    
     if( sum(strcmp(tree.value,attributes)) >0)
-        index = find(ismember(attributes, tree.value) == 1);
-        
+        index = find(ismember(attributes, tree.value) == 1);  % Finds the attribute that is being used to split
         if (instance(1, index) <= tree.bound ); % attribute is true for this instance
         % Recur down the right side
-       
         classifications = ClassifyByTree(tree.right, attributes, instance); 
         else
         % Recur down the left side
-        
-      
         classifications = ClassifyByTree(tree.left, attributes, instance);
         end
     else
-        
         classifications = tree.value;
         return
     end    
