41d8370151c9378953c5c39ea6af0738fb966ce7
Sahith Dambekodi <SND96@users.noreply.github.com>
2016-06-10 11:41:33 +0530

    Updated ClassifyByTree

--- ClassifyByTree.m
+++ ClassifyByTree.m
@@ -1,19 +1,22 @@
-function [classifications] = ClassifyByTree(tree, attributes, instance)
-    attr = categorical(attributes);
-    if( sum(strcmp(tree.value,attr)) >0)
+function classifications = ClassifyByTree(tree, attributes, instance)
+    
+    %fprintf('%d',strcmp(tree.value,attributes));
+    if( sum(strcmp(tree.value,attributes)) >0)
         index = find(ismember(attributes, tree.value) == 1);
+        
         if (instance(1, index) <= tree.bound ); % attribute is true for this instance
         % Recur down the right side
-        fprintf('lef');
+       
         classifications = ClassifyByTree(tree.right, attributes, instance); 
         else
         % Recur down the left side
         
-        fprintf('rig');
+      
         classifications = ClassifyByTree(tree.left, attributes, instance);
         end
     else
-        classifications =tree.value;
+        
+        classifications = tree.value;
         return
     end    
 end
